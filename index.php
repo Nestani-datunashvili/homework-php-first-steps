9849c0ad61ecd766419c200e4ca9fe9ae745bbf9
Nestani Datunashvili <nestani.datunashvili@gmail.com>
2024-09-10 23:24:17 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$name = "hello world";
+$num=28;
+$salary= 1200.5;
+$isLoggedIn=true;
+$data=null;
+$fruits=[`apple`,`bannnana`,`oragne`];
+
+
+$car=["Audi", "BMW", "Mercedes", "Jaguar"];
+
+echo $car[1];
+
+?>
